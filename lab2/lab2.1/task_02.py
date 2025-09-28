d8444c949c164cf7285c001bedeb59fc68413365
k1yoryu <alleksej.sak@gmail.com>
2025-09-28 13:10:04 +0300

    Добавлен вывод уникальных цифр

--- lab2/lab2.1/task_02.py
+++ lab2/lab2.1/task_02.py
@@ -4,9 +4,16 @@ parts = user_input.split()
 numbers = []
 
 for part in parts:
-    num = float(part)
-    if num.is_integer():
-        numbers.append(int(num))
+    value = float(part)
+    if value.is_integer():
+        numbers.append(int(value))
     else:
-        numbers.append(num)
+        numbers.append(value)
 print(numbers)
+
+unique_numbers = []
+for num in numbers:
+    if numbers.count(num) == 1:
+        unique_numbers.append(num)
+print("Уникальные числа: ", *unique_numbers)
+

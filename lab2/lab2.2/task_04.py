511183d22c6e4c346e96b49658d4547187b840d6
k1yoryu <alleksej.sak@gmail.com>
2025-09-28 19:57:37 +0300

    Добавлено транспонирование матрицы

--- lab2/lab2.2/task_04.py
+++ lab2/lab2.2/task_04.py
@@ -1,3 +1,17 @@
+def transpose_matrix(matrix):
+
+    stroki = len(matrix)
+    stolbci = len(matrix[0])
+
+    transposed = []
+    for j in range(stolbci):
+        new_row = []
+        for i in range(stroki):
+            new_row.append(matrix[i][j])
+        transposed.append(new_row)
+
+    return transposed
+
 stroki = int(input("Введите количество строк: "))
 stolbci = int(input("Введите количество столбцов: "))
 
@@ -12,4 +26,10 @@ for i in range(stroki):
 
 print("\nИсходная матрица:")
 for row in matrix:
+    print(row)
+
+transposed = transpose_matrix(matrix)
+
+print("\nТранспонированная матрица:")
+for row in transposed:
     print(row)

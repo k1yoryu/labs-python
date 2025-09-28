f4d510f1f5c7956d9457544d05eefbb00af1a1bf
k1yoryu <alleksej.sak@gmail.com>
2025-09-28 13:31:02 +0300

    Добавлен вывод повторяющихся, отрицательных, четных, нечетных, с плавающей точкой чисел

--- lab2/lab2.1/task_02.py
+++ lab2/lab2.1/task_02.py
@@ -12,8 +12,46 @@ for part in parts:
 print(numbers)
 
 unique_numbers = []
+
 for num in numbers:
     if numbers.count(num) == 1:
         unique_numbers.append(num)
 print("Уникальные числа: ", *unique_numbers)
+
+povtor_numbers = []
+
+for num in numbers:
+    if numbers.count(num) > 1:
+        if num not in povtor_numbers:
+            povtor_numbers.append(num)
+print("Повторяющиеся числа: ", *povtor_numbers)
+
+chetnie_numbers = []
+nechetnie_numbers = []
+
+for num in numbers:
+    if num % 1 == 0:
+        if num % 2 == 0:
+            if num not in chetnie_numbers:
+                chetnie_numbers.append(num)
+        else:
+            if num not in nechetnie_numbers:
+               nechetnie_numbers.append(num)
+print("Чётные числа: ", *chetnie_numbers)
+print("Нечётные числа: ", *nechetnie_numbers)
+
+otricatelnie_numbers = []
+for num in numbers:
+    if num < 0:
+        if num not in otricatelnie_numbers:
+            otricatelnie_numbers.append(num)
+print("Отрицательные числа: ", *otricatelnie_numbers)
+
+float_numbers = []
+for num in numbers:
+    if num % 1 != 0:
+        if num not in float_numbers:
+            float_numbers.append(num)
+print("Числа с плавающей точкой: ", *float_numbers)
+
 

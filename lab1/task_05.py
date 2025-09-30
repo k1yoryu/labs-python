7a8dea73ed0f1a5b4bf3f443c3e2fbcf79ffd825
k1yoryu <alleksej.sak@gmail.com>
2025-09-30 11:54:11 +0300

    Сделано без циклов(ограничение первой работы)

--- lab1/task_05.py
+++ lab1/task_05.py
@@ -1,10 +1,17 @@
 number = int(input("Введите число: "))
+
 if number % 7 == 0:
-    print("Магическое число!")
+    result = "Магическое число!"
 else:
-    summa = 0
-    while number > 0:
-        cifra = number % 10
-        summa += cifra
-        number = number // 10
-    print(summa)
+    if number < 10:
+        result = number
+    elif number < 100:
+        result = (number // 10) + number % 10
+    elif number < 1000:
+        result = (number // 100) + (number // 10) % 10 + number % 10
+    elif number < 10000:
+        result = (number // 1000) + (number // 100) % 10 + (number // 10) % 10 + number % 10
+    else:
+        result = "Слишком большое число!"
+
+print(result)

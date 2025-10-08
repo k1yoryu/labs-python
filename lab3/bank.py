ebc82ba406adbe2262c4d5d9402c3d038d13feb1
Aleksey Sak <alleksej.sak@gmail.com>
2025-10-08 21:28:18 +0300

    Добавлен класс Client с методами для управления клиентом и его счетами: инициализация  добавление, удаление и получение счетов ...

--- lab3/bank.py
+++ lab3/bank.py
@@ -1,4 +1,6 @@
 from exceptions import InsufficientFundsError
+from exceptions import AccountExistsError, AccountNotFoundError
+
 
 class BankAccount:
     def __init__(self, account_id, client_id, currency, balance=0.0):
@@ -28,3 +30,52 @@ class BankAccount:
 
     def __str__(self):
         return f"Счет {self.account_id}: {self.currency} {self.balance:.2f}"
+
+
+class Client:
+        def __init__(self, client_id, name, email=None, phone=None):
+            self.client_id = client_id
+            self.name = name
+            self.email = email
+            self.phone = phone
+            self.accounts = {}
+
+        def add_account(self, account):
+            if account.currency in self.accounts:
+                raise AccountExistsError("Счет в этой валюте уже существует")
+            self.accounts[account.currency] = account
+
+        def remove_account(self, currency):
+            if currency not in self.accounts:
+                raise AccountNotFoundError("Счет не найден")
+            del self.accounts[currency]
+
+        def get_account(self, currency):
+            if currency not in self.accounts:
+                raise AccountNotFoundError("Счет не найден")
+            return self.accounts[currency]
+
+        def to_dict(self):
+            return {
+                "id": self.client_id,
+                "name": self.name,
+                "email": self.email,
+                "phone": self.phone,
+                "accounts": [a.to_dict() for a in self.accounts.values()]
+            }
+
+        @staticmethod
+        def from_dict(data):
+            client = Client(data["id"], data["name"], data.get("email"), data.get("phone"))
+            for acc_data in data.get("accounts", []):
+                acc = BankAccount(
+                    acc_data["id"],
+                    client.client_id,
+                    acc_data["currency"],
+                    acc_data["balance"]
+                )
+                client.add_account(acc)
+            return client
+
+        def __str__(self):
+            return f"{self.name} (ID: {self.client_id})"

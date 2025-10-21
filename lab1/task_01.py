34f49c921a0f5a6ab77fd7758b0591e146004dba
k1yoryu <alleksej.sak@gmail.com>
2025-10-21 13:10:58 +0300

    Задание через f-строки

--- lab1/task_01.py
+++ lab1/task_01.py
@@ -5,5 +5,6 @@ otchestvo = input("Введите отчество: ")
 if not surname or not name or not otchestvo:
     print("Ошибка: все поля должны быть заполнены!")
 else:
-    result = surname + " " + name[0] + "." + otchestvo[0] + "."
-    print("Результат:", result)
+    # result = surname + " " + name[0] + "." + otchestvo[0] + "."
+    # print("Результат:", result)
+    print(f"{surname} {name[0]}.{otchestvo[0]}.")

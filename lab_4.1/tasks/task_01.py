e1b879a7af1a054edb26ae377f036b2130c2c018
Aleksey Sak <alleksej.sak@gmail.com>
2025-10-25 20:09:25 +0300

    построение графиков функций

--- lab_4.1/tasks/task_01.py
+++ lab_4.1/tasks/task_01.py
@@ -1,1 +1,11 @@
-#0
+import matplotlib.pyplot as plt
+import numpy as np
+
+x_grad = np.linspace(-360, 360, 1000)
+x_rad = np.radians(x_grad)
+
+f_x = np.exp(np.cos(x_rad)) + np.log(np.cos(0,6 * x_rad) ** 2 + 1) * np.sin(x_rad)
+h_x = -np.log((np.cos(x_rad) + np.sin(x_rad)) ** 2 + 2.5) + 10
+plt.plot(x_grad, f_x)
+plt.plot(x_grad, h_x)
+plt.show()

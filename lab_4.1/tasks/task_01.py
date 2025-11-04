e33b11861f239a4263ccaa9f55f2adfb648ee92e
k1yoryu <alleksej.sak@gmail.com>
2025-11-04 13:10:43 +0300

    add оформление графика

--- lab_4.1/tasks/task_01.py
+++ lab_4.1/tasks/task_01.py
@@ -4,8 +4,16 @@ import numpy as np
 x_grad = np.linspace(-360, 360, 1000)
 x_rad = np.radians(x_grad)
 
-f_x = np.exp(np.cos(x_rad)) + np.log(np.cos(0,6 * x_rad) ** 2 + 1) * np.sin(x_rad)
+f_x = np.exp(np.cos(x_rad)) + np.log(np.cos(0.6 * x_rad) ** 2 + 1) * np.sin(x_rad)
 h_x = -np.log((np.cos(x_rad) + np.sin(x_rad)) ** 2 + 2.5) + 10
-plt.plot(x_grad, f_x)
-plt.plot(x_grad, h_x)
+
+plt.plot(x_grad, f_x, label='f(x)')
+plt.plot(x_grad, h_x, label='h(x)')
+
+plt.title("Графики функций f(x) и h(x)")
+plt.xlabel("x (градусы)")
+plt.ylabel("Значения функкий f(x), h(x)")
+plt.legend()
+plt.grid(True)
+plt.tight_layout()
 plt.show()

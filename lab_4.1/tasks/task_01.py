ac4f4489bedf3dac8e17c64fab0448d31f472936
k1yoryu <alleksej.sak@gmail.com>
2025-11-04 13:24:06 +0300

    end

--- lab_4.1/tasks/task_01.py
+++ lab_4.1/tasks/task_01.py
@@ -1,18 +1,28 @@
+import math
 import matplotlib.pyplot as plt
-import numpy as np
 
-x_grad = np.linspace(-360, 360, 1000)
-x_rad = np.radians(x_grad)
+x_grad = [x for x in range(-360, 361)]
+x_rad = [math.radians(x) for x in x_grad]
 
-f_x = np.exp(np.cos(x_rad)) + np.log(np.cos(0.6 * x_rad) ** 2 + 1) * np.sin(x_rad)
-h_x = -np.log((np.cos(x_rad) + np.sin(x_rad)) ** 2 + 2.5) + 10
+f_x = []
+h_x = []
+
+for x in x_rad:
+    cos_x = math.cos(x)
+    sin_x = math.sin(x)
+    cos_06x = math.cos(0.6 * x)
+
+    f = math.exp(cos_x) + math.log(cos_06x ** 2 + 1) * sin_x
+    h = -math.log((cos_x + sin_x) ** 2 + 2.5) + 10
+
+    f_x.append(f)
+    h_x.append(h)
 
 plt.plot(x_grad, f_x, label='f(x)')
 plt.plot(x_grad, h_x, label='h(x)')
-
 plt.title("Графики функций f(x) и h(x)")
 plt.xlabel("x (градусы)")
-plt.ylabel("Значения функкий f(x), h(x)")
+plt.ylabel("Значения функций f(x), h(x)")
 plt.legend()
 plt.grid(True)
 plt.tight_layout()

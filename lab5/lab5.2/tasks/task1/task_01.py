ad70034c26ae71bdd9b957b0424b83e888ae1d2a
Aleksey Sak <alleksej.sak@gmail.com>
2025-11-20 21:02:16 +0300

    подсчет слов

--- lab5/lab5.2/tasks/task1/task_01.py
+++ lab5/lab5.2/tasks/task1/task_01.py
@@ -1,31 +1,5 @@
-import pytest
-
 def count_words(stroka):
     if stroka is None:
         return 0
     words = stroka.split()
     return len(words)
-
-def test_simple_sentence():
-    # Проверка обычного предложения
-    assert count_words("Привет мир") == 2
-
-def test_sentence_with_multiple_spaces():
-    # Проверка предложения с лишними пробелами между словами
-    assert count_words("Python    это   круто") == 3
-
-def test_sentence_with_leading_trailing_spaces():
-    # Проверка пробелов в начале и конце строки
-    assert count_words("   Начало и конец   ") == 3
-
-def test_empty_string():
-    # Проверка пустой строки
-    assert count_words("") == 0
-
-def test_string_with_only_spaces():
-    # Проверка строки, состоящей только из пробелов
-    assert count_words("     ") == 0
-
-def test_none_input():
-    # Проверка случая, если передали None (защита от ошибок)
-    assert count_words(None) == 0

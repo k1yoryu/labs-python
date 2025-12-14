31732d490c00843f82fb934ae2abfb55620e5381
k1yoryu <alleksej.sak@gmail.com>
2025-12-14 23:18:03 +0300

    add fixture, marker

--- lab5/lab5.2/test_task/test_task.py
+++ lab5/lab5.2/test_task/test_task.py
@@ -30,13 +30,31 @@ def test_random_poryadok():
     assert proverka_treygolnika(5, 3, 4) == True
     assert proverka_treygolnika(4, 5, 3) == True
 
-def test_string():
-    with pytest.raises(TypeError):
-        proverka_treygolnika("3", "4", "5")
+@pytest.fixture
+def first_tex():
+    return (3, 4, 5)
 
-    with pytest.raises(TypeError):
-        proverka_treygolnika(3, "4", 5)
+def test_fixture(first_tex):
+    a, b, c = first_tex
+    assert proverka_treygolnika(a, b, c) == True
 
-def test_invalid_types():
-    with pytest.raises(TypeError):
-        proverka_treygolnika(None, 1, 1)
+
+@pytest.mark.parametrize("a,b,c", [(3, 4, 5),(5, 12, 13),])
+def test_pramoygolnie_parametrized(a, b, c):
+    assert proverka_treygolnika(a, b, c) is True
+
+@pytest.mark.skip(reason="Пропущен")
+def test_kakaya_to_textura():
+    assert proverka_treygolnika(7, 24, 25) == True
+
+@pytest.mark.xfail(run=False)
+def test_kakaya_to_textura_fail():
+    assert proverka_treygolnika(3, 4, 5) == True
+
+@pytest.mark.xfail(strict=True)
+def test_strict():
+    assert proverka_treygolnika(3, 4, 5) == False
+
+@pytest.mark.xfail(raises=ValueError)
+def test_ValueError():
+    assert proverka_treygolnika(42, 42, 42.0) == False

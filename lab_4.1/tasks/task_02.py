8649636b213aaf2e863d9ff8eed2f9dc6a118f8e
k1yoryu <alleksej.sak@gmail.com>
2025-12-15 06:22:43 +0300

    Убран while для точек разрыва; логика разрыва через методы библиотеки

--- lab_4.1/tasks/task_02.py
+++ lab_4.1/tasks/task_02.py
@@ -1,45 +1,25 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 def f(x):
     return 5 / (x**2 - 9)
 
-all_segments = []
+x1 = np.linspace(-10, -3.01, 500)
+x2 = np.linspace(-2.99, 2.99, 500)
+x3 = np.linspace(3.01, 10, 500)
 
-x = -10.0
-xs, ys = [], []
-while x < -3.0:
-    xs.append(x)
-    ys.append(f(x))
-    x += 0.05
-all_segments.append((xs, ys))
-
-x = -2.99
-xs, ys = [], []
-while x < 3.0:
-    xs.append(x)
-    ys.append(f(x))
-    x += 0.05
-all_segments.append((xs, ys))
-
-x = 3.01
-xs, ys = [], []
-while x <= 10.0:
-    xs.append(x)
-    ys.append(f(x))
-    x += 0.05
-all_segments.append((xs, ys))
-
-for xs, ys in all_segments:
-    plt.plot(xs, ys, 'b-')
+plt.plot(x1, f(x1))
+plt.plot(x2, f(x2))
+plt.plot(x3, f(x3))
 
 plt.axhline(0, color='black', linewidth=0.5)
 plt.axvline(0, color='black', linewidth=0.5)
+plt.axvline(x=3, color='red', linestyle='--', linewidth=0.5)
+plt.axvline(x=-3, color='red', linestyle='--', linewidth=0.5)
 plt.grid(True, linestyle='--', alpha=0.7)
 plt.title(r"График функции $f(x) = \frac{5}{x^2 - 9}$")
 plt.xlabel("x")
 plt.ylabel("f(x)")
-plt.axvline(x=3, color='red', linestyle='--', linewidth=0.5)
-plt.axvline(x=-3, color='red', linestyle='--', linewidth=0.5)
 plt.xlim(-10, 10)
 plt.ylim(-10, 10)
 plt.show()
